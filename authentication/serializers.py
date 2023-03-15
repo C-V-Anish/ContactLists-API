8e305fdfc74c647746d58b3ea04fa7bc56009ffa
C V Anish <mahadevrao77@gmail.com>
2023-03-16 03:06:44 +0530

    JWT Authentication

---
+++ authentication/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    first_name=serializers.CharField(min_length=6,max_length=20)
+    last_name=serializers.CharField(min_length=6,max_length=10)
+    # username=serializers.CharField(min_length=12,max_length=32)
+    email=serializers.EmailField(min_length=15)
+    password=serializers.CharField(min_length=8,write_only=True)
+
+    class Meta:
+        model=User
+        fields=['first_name','username','last_name','email','password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+    def validate(self, attrs):
+        email=attrs.get('email','')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({'msg':'Email already in use'})
+        return super().validate(attrs)
+
+    
+
+
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer,User
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+class RegisterView(GenericAPIView):
+    serializer_class=UserSerializer
+    queryset=User.objects.all()
+
+    def post(self,request):
+        serializer=UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
 
